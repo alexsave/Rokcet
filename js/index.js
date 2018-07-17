9b0a686800a0e704e9f14614da3b77be4f88ad43
Alex Saveliev <alexvsaveliev@gmail.com>
2018-07-17 19:02:43 -0400

    fixed map

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 function initMap() {
-    // The location of Uluru
     var durham = {lat: 43.136, lng: -70.926};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: durham});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: durham, map: map});
+
+    var mOptions =
+    {
+        zoom: 15,
+        center: durham,
+        disableDefaultUI: true
+    }
+
+    var map = new google.maps.Map( document.getElementById('map'), mOptions);
+
+    //var marker = new google.maps.Marker({position: durham, map: map});
 }
