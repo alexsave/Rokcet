326a38bdfe341f8f9c321b1c89d60e62b018dff0
Alex Saveliev <alexvsaveliev@gmail.com>
2018-07-27 16:37:38 -0400

    stupid typo

--- php/phpsqlinfo_addrow.php
+++ php/phpsqlinfo_addrow.php
@@ -33,7 +33,7 @@ if(!$result)
     die('Invalid query: ' . mysqli_error($connection));
 }
 
-header('Content-Type: text/event-steam');
+header('Content-Type: text/event-stream');
 header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
