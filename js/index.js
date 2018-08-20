f02f16a1bf006d2ac3b06fb7281312259ed4d2b8
Alex Saveliev <alexvsaveliev@gmail.com>
2018-08-20 14:54:13 -0400

    got rid of connection close

--- js/index.js
+++ js/index.js
@@ -464,7 +464,7 @@ function downloadPost(url, params, callback)
     let request = new XMLHttpRequest;
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.setRequestHeader("Content-length", params.length);
-    request.setRequestHeader("Connection", "close");
+
     request.onreadystatechange = function()
     {
         if(request.readyState === 4)
